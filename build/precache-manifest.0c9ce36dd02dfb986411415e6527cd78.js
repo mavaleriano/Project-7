aa70edf8be8b6f386bf1e9f53fed17b4445f64a0
Miguel <miasvaleriano@gmail.com>
2020-05-20 14:55:56 -0400

    changing trying to change options to fix error

---
+++ build/precache-manifest.0c9ce36dd02dfb986411415e6527cd78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b44ce84665460e09b5f9e2d062f1c2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c70dc3d8b5e29e8151c9",
+    "url": "/static/css/main.25aa6c7b.chunk.css"
+  },
+  {
+    "revision": "0ec86fc25245c2e49f08",
+    "url": "/static/js/2.af456fb8.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.af456fb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c70dc3d8b5e29e8151c9",
+    "url": "/static/js/main.4cfda8e7.chunk.js"
+  },
+  {
+    "revision": "3037271ad67899dbbe42",
+    "url": "/static/js/runtime-main.0b4b304b.js"
+  },
+  {
+    "revision": "1691d59fe4a1042225faa9ecfc2c7c2b",
+    "url": "/static/media/404img1.1691d59f.png"
+  }
+]);
